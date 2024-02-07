870a06653e633290d543446e39f1f70db735c246
Nicolas Heinig <nicolas.heinig@gmail.com>
2024-02-07 16:13:19 +0100

    Add function to switch the mysql instances

--- main-compose.sh
+++ main-compose.sh
@@ -1,3 +1,15 @@
 function main-compose() {
   docker-compose -f ~/projects/main-compose/docker-compose.yml "$@"
 }
+
+function switch-mysql() {
+  echo 'Switching mysql instances'
+  if main-compose ps | grep -q 'mysql-5-7'
+  then
+    main-compose down mysql-5-7
+    main-compose up -d mysql-8
+  else
+    main-compose down mysql-8
+    main-compose up -d mysql-5-7
+  fi
+}
